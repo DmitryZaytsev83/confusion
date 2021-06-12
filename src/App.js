b4f7413af26a764f1e5688625a177abd2681a200
Dmitry <dmitry.zaytsev83@gmail.com>
2021-06-12 08:20:38 +0300

    add MenuComponent and images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import MenuComponent from "./components/MenuComponent";
 
 function App() {
     return (
         <div className="App">
-            <h1>hello</h1>
+            <MenuComponent></MenuComponent>
         </div>
     );
 }
