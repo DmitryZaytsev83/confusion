065f7a224f54f8247fb5b116c366161e2b230860
Dmitry <dmitry.zaytsev83@gmail.com>
2021-06-14 12:02:33 +0300

    Components part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import MenuComponent from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
+import { Navbar, NavbarBrand } from "reactstrap";
 
 function App() {
     return (
         <div className="App">
-            <MenuComponent></MenuComponent>
+            <Navbar color="primary" dark>
+                <div className="container">
+                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                </div>
+            </Navbar>
+            <MenuComponent dishes={DISHES}/>
         </div>
     );
 }
