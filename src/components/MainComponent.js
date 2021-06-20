31211362e325860c9b2f4867717726c307081455
Dmitry <dmitry.zaytsev83@gmail.com>
2021-06-20 12:30:21 +0300

    Single page application part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { Switch, Route, Redirect } from 'react-router-dom';
 import HomeComponent from "./HomeComponent";
 import MenuComponent from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
+import ContactComponent from "./ContactComponent";
 import HeaderComponent from "./HeaderComponent";
 import FooterComponent from "./FooterComponent";
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { DISHES } from "../shared/dishes";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
+
+const HomePage = (props) => {
+    const dish = DISHES.filter((dish) => dish.featured)[0];
+    const promotion = PROMOTIONS.filter((promo) => promo.featured)[0];
+    const leader = LEADERS.filter((leader) => leader.featured)[0];
+    return (
+        <HomeComponent dish={dish} promotion={promotion} leader={leader}/>
+    )
+}
 
 function Main() {
     return (
         <div className="Main">
             <HeaderComponent/>
             <Switch>
-                <Route path="/home" component={HomeComponent}/>
+                <Route path="/home" component={HomePage}/>
                 <Route exact path="/menu" component={() => <MenuComponent dishes={DISHES}/>}/>
+                <Route exact path="/contactus" component={() => <ContactComponent/>}/>
                 <Redirect to="/home"/>
             </Switch>
             <FooterComponent/>
